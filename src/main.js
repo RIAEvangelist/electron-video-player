7a17711a29f306d7252ceaf04de69c541afd8202
Giles Roadnight <giles@roadnight.name>
2016-07-23 17:15:25 +0100

    Fix to make it work with Electron 1.0+

--- src/main.js
+++ src/main.js
@@ -1,48 +1,63 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
-require('crash-reporter').start();
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
 
-var player = null;
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+		  title:'Electron Video Player',
+		  'accept-first-mouse':true,
+		  width: 640,
+		  height: 480,
+		  'min-width': 640,
+		  'min-height': 480,
+		  frame:false,
+		  icon:__dirname+'/img/logo-256.png',
+		  'text-areas-are-resizable':false
+	  });
 
-app.on(
-    'window-all-closed',
-    function() {
-        if (process.platform != 'darwin') {
-            app.quit();
-        }
-    }
-);
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/player.html`)
 
-app.on(
-    'ready',
-    function() {
-        player = new BrowserWindow(
-            {
-                title:'Electron Video Player',
-                'accept-first-mouse':true,
-                width: 640,
-                height: 480,
-                'min-width': 640,
-                'min-height': 480,
-                frame:false,
-                icon:__dirname+'/img/logo-256.png',
-                'text-areas-are-resizable':false
-            }
-        );
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
 
-        player.loadUrl('file://' + __dirname + '/player.html');
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
 
-        //player.openDevTools();
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
-        player.on(
-            'closed',
-            function() {
-                player = null;
-            }
-        );
-    }
-);
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
 
-
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
